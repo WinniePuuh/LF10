0864511f828e3d343dc307826f67ce2cfe73ade9
Pierre Dennert <pierre@dennert.me>
2015-01-05 18:04:19 +0100

    Umlaute gefixt
    Umlaute gefixt

--- PICCCheck/PICCCheck.ino
+++ PICCCheck/PICCCheck.ino
@@ -46,15 +46,15 @@ void setup()
   // Initialisierung, weils toll aussieht!
   setColor(255, 0, 0); // Rot
   delay(500);
-  setColor(0, 255, 0); // Gr�n
+  setColor(0, 255, 0); // Grün
   delay(300);
   setColor(0, 0, 255); // Blau
   delay(600);
-  setColor(255, 255, 255); // Wei�
+  setColor(255, 255, 255); // Weiß
   delay(400);
   setColor(255, 255, 0); // Gelb
   delay(600);
-  setColor(0, 255, 255); // T�rkis
+  setColor(0, 255, 255); // Türkis
   delay(500);
 }
  
@@ -66,7 +66,7 @@ void loop()
       // RGB LED aus
       setColor(0, 0, 0); // Schwarz
 
-    // F�r einen sch�neren �bergang
+    // Für einen schöneren Übergang
     delay(100);
     
     // Nach neuen Karten suchen.
@@ -76,19 +76,19 @@ void loop()
       return;
     }
 
-  // Eine der gefunden Karten ausw�hlen
+  // Eine der gefunden Karten auswählen
   if ( ! mfrc522.PICC_ReadCardSerial())
   {
     return;
   }
 
-  // Eigene UID bilden. Dazu nehmen wir die 4(7)Byte Hex UID aus Block 0 und quetschen sie in einen sch�nen String.
+  // Eigene UID bilden. Dazu nehmen wir die 4(7)Byte Hex UID aus Block 0 und quetschen sie in einen schönen String.
   String uID = String(mfrc522.uid.uidByte[0],HEX)+String(mfrc522.uid.uidByte[1],HEX)+String(mfrc522.uid.uidByte[2],HEX)+String(mfrc522.uid.uidByte[3],HEX);
   
   // PICC Typ herausbekommen
   byte piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
 
-/* 
+
   //Debug Ausgabe
   Serial.println();
   Serial.print("Die Kartennummer lautet: ");
@@ -97,27 +97,28 @@ void loop()
   Serial.print("Picc Type: ");
   Serial.println(mfrc522.PICC_GetTypeName(piccType));
   Serial.println();
-*/
+
 /* 
-// �berpr�fe auf unterst�tzten PICC Typ und noch mehr Debug Kram
+// Überprüfe auf unterstützten PICC Typ und noch mehr Debug Kram
   if ( piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K&& piccType != MFRC522::PICC_TYPE_MIFARE_1K) {
-    Serial.println("Diese Tech-Demo ben�tigt MIFARE Classic Karten.");
-    // RGB LED f�r 3,5sek auf BLAU
+    Serial.println("Diese Tech-Demo benötigt MIFARE Classic Karten.");
+    // RGB LED für 3,5sek auf BLAU
    setColor(0,0,255); // blau
     delay(3500);
     return;
   }
  */
-// Den UID String vergleichen und eine entsprechende Aktion ausf�hren
+// Den UID String vergleichen und eine entsprechende Aktion ausführen
   if( uID=="5b19d5ec" || uID=="4664d835")  {
      Serial.println("Access granted for uID " + uID);
-    // RGB LED f�r 3,5sek auf GR�N
-    setColor(0, 255, 0); // gr�n
+    // RGB LED für 3,5sek auf GRÜN
+    setColor(0, 255, 0); // grün
     delay(3500);  
-}
-else {
+  }
+  
+  else {
        Serial.println("I can't let you do that Dave (" + uID + ")");
-      // RGB LED f�r 3,5sek auf ROT
+      // RGB LED für 3,5sek auf ROT
       setColor(255, 0, 0); // rot
       delay(3500);
   }
